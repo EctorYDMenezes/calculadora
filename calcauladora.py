8de58668c9360a06bfa0e0e0743e33250e8ed345
Ector <ectoryuriduarte@gmail.com>
2024-09-10 17:34:53 -0300

    feat: criacao da funcao multiplicacao

--- calcauladora.py
+++ calcauladora.py
@@ -2,3 +2,5 @@ def soma(x,y):
     return "O resultado da soma é igual à" + (x+y)
 def subtracao(x,y):
     return "O resultado da subtracao é igual à" + (x-y)
+def multiplicacao(x,y):
+    return "O resultado da multiplicacao é igual à" + (x*y)
