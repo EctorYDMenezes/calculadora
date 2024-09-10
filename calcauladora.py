2557f202c74c6148192a29edab768cc8f7798ce6
Ector <ectoryuriduarte@gmail.com>
2024-09-10 17:36:55 -0300

    feat: criacao da funcao divisao

--- calcauladora.py
+++ calcauladora.py
@@ -4,3 +4,8 @@ def subtracao(x,y):
     return "O resultado da subtracao é igual à" + (x-y)
 def multiplicacao(x,y):
     return "O resultado da multiplicacao é igual à" + (x*y)
+def divisao(x,y):
+    if(y == 0):
+        return "Divisao por 0 ERROR"
+    else:
+        return "O resultado da multiplicacao é igual à" + (x/y)
